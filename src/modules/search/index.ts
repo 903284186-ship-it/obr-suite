30cdc98901334ed15c1dfa4f0e2edfc03b1b136f
曾宥崴 <zengyouwei1@crop.netease.com>
2026-05-06 16:00:27 +0800

    drag-fix

--- src/modules/search/index.ts
+++ src/modules/search/index.ts
@@ -122,7 +122,6 @@ async function openBar(): Promise<void> {
     const h_q = hAnchor === "LEFT" ? "left" : "right";
     const v_q = vAnchor === "TOP" ? "top" : "bottom";
     await OBR.popover.open({
-      anchorReference: "POSITION",
       id: POPOVER_ID,
       url: `${URL}?h=${h_q}&v=${v_q}`,
       width: w,
